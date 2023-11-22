8494c4025fdb6d769b9c328d3b8e9c9739b0e11a
Houcinetb <tiabh16@gmail.com>
2023-11-22 01:48:32 -0800

    add function

--- print-function.c
+++ print-function.c
@@ -36,3 +36,33 @@ void print_prct(int *char_num)
 			char_num++;
 
 }
+
+{
+
+int print_int(va_list ap)
+{
+        int i;
+        i =va_arg(ap, int);
+        _putchar(i);
+
+        return(1);
+}
+
+{
+
+int p_int_rec(long int n, int count)
+{
+	int nr_digits = count;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (-1) * n;
+	}
+	if (n / 10)
+	{
+		nr_digits = p_int_rec(n / 10, count + 1);
+	}
+	_putchar(n % 10 + '0');
+	return (nr_digits);
+}
